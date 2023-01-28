8a50638bcfe1a4c9e39e5757a176c1097daaafdc
adeel <adeelsk4@gmail.com>
2023-01-28 19:59:03 +0000

    widgets 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,56 @@
 import 'package:flutter/material.dart';
+import './question.dart';
+import './answer.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  final questions = const [
+    {
+      'questionText': '1: Whats your fav color?',
+      'answers': ['Red', 'Blue', 'Green', 'Purple'],
+    },
+    {
+      'questionText': '1: Whats your fav Actor?',
+      'answers': ['Angie', 'Brad', 'Colin', 'Javier'],
+    },
+    {
+      'questionText': '1: Whats your fav Animal?',
+      'answers': ['Dog', 'Cat', 'Bear', 'Rhino'],
+    },
+  ];
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+
+    print(_questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      'What\'s your favourite food?',
-      'What\'s your favourite animal? ',
-    ];
     return MaterialApp(
       home: Scaffold(
           appBar: AppBar(
             title: Text('My First APP'),
           ),
           body: Column(
-            children: <Widget>[],
+            children: [
+              Question(questions[_questionIndex]['questionText']),
+              ...(questions[_questionIndex]['answers'] as List<String>)
+                  .map((answer) {
+                return Answer(_answerQuestion, answer);
+              }).toList()
+            ],
           )),
     );
   }
